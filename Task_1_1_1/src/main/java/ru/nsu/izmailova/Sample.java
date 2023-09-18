49321a6a8f513df956f35fee4f2cf17af44ea68b
izm-ka <izm.karina@gmail.com>
2023-09-18 17:57:40 +0700

    add comments

--- Task_1_1_1/src/main/java/ru/nsu/izmailova/Sample.java
+++ Task_1_1_1/src/main/java/ru/nsu/izmailova/Sample.java
@@ -1,12 +1,27 @@
 package ru.nsu.izmailova;
 
+/**
+ * this class contains the heap sort algorithm
+ */
 public class Sample {
+    /**
+     * method to swap two elements.
+     * @param arr the array that elements are swapping
+     * @param i index of the first element
+     * @param j index of the second element
+     */
     public static void swap(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
 
+    /**
+     * method to create a max heap
+     * @param arr heap
+     * @param n the size of the heap
+     * @param i index of the root of the subtree
+     */
     public static void heap(int[] arr, int n, int i) {
         int parent = i;
         int lChild = 2 * i + 1;
@@ -27,15 +42,21 @@ public class Sample {
         }
     }
 
+    /**
+     * main method to sort an array
+     * @param arr array to be sorted
+     */
     public static void heapSort(int[] arr)
     {
         int n = arr.length;
         int root = 0;
 
+        //create a max heap
         for (int i = n / 2 - 1; i >= 0; i--) {
             heap(arr, n, i);
         }
 
+        //extract the maximum element and re-heapify
         for (int i = n - 1; i >= 0; i--) {
             swap(arr, i, root);
             heap(arr, i, root);
