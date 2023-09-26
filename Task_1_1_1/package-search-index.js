2a22d8aa00b15efe92f0d2809e1009221e4bbadd
izm-ka <izm-ka@users.noreply.github.com>
2023-09-26 05:00:07 +0000

    Deploying to gh-pages from @ izm-ka/OOP@38295237cf052d08ee94c492ad50e5feecb7b2ee 🚀

--- Task_1_1_1/package-search-index.js
+++ Task_1_1_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.nsu.izmailova"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.nsu.izmailova.heapsort"}]
--- Task_1_1_1/type-search-index.js
+++ Task_1_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.izmailova","l":"Sample"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.izmailova.heapsort","l":"heapSort"}]
