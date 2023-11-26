91c42cf155c027674d68577d586c10503060603e
izm-ka <izm.karina@gmail.com>
2023-11-26 14:19:58 +0700

    hide dirs

--- Task_1_2_1/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.izmailova.tree","l":"Tree.Search"},{"p":"ru.nsu.izmailova.tree","l":"Tree"},{"p":"ru.nsu.izmailova.tree","l":"TreeTest"}];updateSearchResults();
