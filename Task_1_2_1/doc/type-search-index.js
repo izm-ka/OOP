48818bcf830c55133e9ee556d6cbb168207085fb
izm-ka <izm.karina@gmail.com>
2023-11-26 14:24:26 +0700

    edit

---
+++ Task_1_2_1/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.izmailova.tree","l":"Tree.Search"},{"p":"ru.nsu.izmailova.tree","l":"Tree"},{"p":"ru.nsu.izmailova.tree","l":"TreeTest"}];updateSearchResults();
