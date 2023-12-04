5f5bf5d68b71d035a4059519aed599fc2551f95c
izm-ka <izm.karina@gmail.com>
2023-12-04 19:07:58 +0700

    rm dir task_1_4_1

--- Task_1_4_1/script.bat
+++
@@ -1,3 +0,0 @@
-javac src/main/java/ru/nsu/izmailova/tree/Tree.java -d ./build
-javadoc -d build/docs -sourcepath src/main/java -subpackages ru.nsu.izmailova.tree
-java -cp ./build ru.nsu.izmailova.tree.Tree
