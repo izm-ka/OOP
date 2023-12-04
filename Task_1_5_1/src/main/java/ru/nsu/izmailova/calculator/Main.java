85bce435e9e2db1c195b12d0b2cbebac8fad016e
izm-ka <izm.karina@gmail.com>
2023-12-04 18:31:58 +0700

    Task_1_5_1

---
+++ Task_1_5_1/src/main/java/ru/nsu/izmailova/calculator/Main.java
@@ -0,0 +1,41 @@
+package ru.nsu.izmailova.calculator;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
+/**
+ * Main function for input an expression.
+ */
+public class Main {
+
+    /**
+     * Main static method to start calculator.
+     */
+    public static void main(String[] args) {
+        System.out.println("CALCULATOR");
+        System.out.println("Use Q to exit.");
+
+        try (BufferedReader input = new BufferedReader(
+                new InputStreamReader(System.in))) {
+
+            while (true) {
+                System.out.print("Enter your expression: ");
+                List<String> expression = List.of(input.readLine().split(" "));
+                if (expression.get(0).equals("Q")) {
+                    break;
+                }
+                try {
+                    var calc = new Calculator(expression);
+                    double result = calc.calculate();
+                    System.out.println("Result: " + result);
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
