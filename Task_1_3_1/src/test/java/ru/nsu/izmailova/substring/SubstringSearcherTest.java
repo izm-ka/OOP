7d0877bce2c2679bf3c4eb17cf05df410edf4dc9
Karina <73380467+izm-ka@users.noreply.github.com>
2023-12-25 17:03:17 +0700

    Task_1_3_1 (#4)
    * add
    
    * add
    
    * first version of HeapSort
    
    * edit
    
    * edit
    
    * edit gitigore
    
    * edit gitignore
    
    * edit gitignore
    
    * edit gitignore
    
    * edit gitignore
    
    * delete .idea
    
    * edited jacoco
    
    * edit checkstyle
    
    * add tests (oneElement,empty,random,big,negative)
    
    * edit some checkstyle
    
    * fix errors
    
    * add comments
    
    * add doc folder
    
    * add docs
    
    * fixed errors
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix and add script
    
    * final edit
    
    * hide idea
    
    * create new branch
    
    * new branch
    
    * firs version
    
    * added tests
    
    * add script and docs
    
    * delete dir Task_1_1_1
    
    * add gitignore
    
    * hide some folders
    
    * hide some files
    
    * edit
    
    * add gradlew
    
    * add gradle
    
    * remove doc
    
    * add equals
    
    * edit equals
    
    * edit
    
    * edit
    
    * edit checkstyle and add @NotNull
    
    * final edit of checkstyle
    
    * substring
    
    * delete
    
    * add 1 simple test and finish the first version of code
    
    * add huge, coding and repeat tests
    
    * edit codestyle
    
    * edit codestyle
    
    * javadoc
    
    * add test with empty file
    
    * edit HugeTest

---
+++ Task_1_3_1/src/test/java/ru/nsu/izmailova/substring/SubstringSearcherTest.java
@@ -0,0 +1,91 @@
+package ru.nsu.izmailova.substring;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.RandomAccessFile;
+import java.nio.channels.Channels;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test class for the SubstringSearcher using JUnit assertions.
+ */
+public class SubstringSearcherTest {
+    @Test
+    public void simpleTest() throws IOException {
+        try (InputStream stream =
+                     getClass().getClassLoader().getResourceAsStream("simple.txt")) {
+            SubstringSearcher alg = new SubstringSearcher(stream);
+            List<Integer> expected = List.of(2);
+            List<Integer> actual = alg.rabinKarp("dad");
+
+            Assertions.assertEquals(expected, actual);
+        }
+    }
+
+    @Test
+    public void hugeTest() throws IOException {
+        try (RandomAccessFile f =
+                     new RandomAccessFile("HugeTest.txt", "rw")) {
+            f.setLength(2000000L);
+            f.seek(2341);
+            f.writeBytes("test");
+            f.seek(12312);
+            f.writeBytes("test");
+            f.seek(1558188);
+            f.writeBytes("test");
+            f.seek(0);
+
+            SubstringSearcher alg = new SubstringSearcher(
+                    Channels.newInputStream(f.getChannel()));
+            String subline = "test";
+
+            List<Integer> act = alg.rabinKarp(subline);
+            List<Integer> exp = Arrays.asList(2341, 12312, 1558188);
+
+            Assertions.assertEquals(exp, act);
+        } finally {
+            File fileToDelete = new File("HugeTest.txt");
+            fileToDelete.delete();
+        }
+    }
+
+    @Test
+    public void repeatTest() throws IOException {
+        try (InputStream stream =
+                     getClass().getClassLoader().getResourceAsStream("repeatTest.txt")) {
+            SubstringSearcher alg = new SubstringSearcher(stream);
+            List<Integer> expected = List.of(0, 2, 4, 6, 8, 10);
+            List<Integer> actual = alg.rabinKarp("ahaha");
+
+            Assertions.assertEquals(expected, actual);
+        }
+    }
+
+    @Test
+    public void codingTest() throws IOException {
+        try (InputStream stream =
+                     getClass().getClassLoader().getResourceAsStream("codingTest.txt")) {
+            SubstringSearcher alg = new SubstringSearcher(stream);
+            List<Integer> expected = List.of(0, 8, 16);
+            List<Integer> actual = alg.rabinKarp("ィ");
+
+            Assertions.assertEquals(expected, actual);
+        }
+    }
+
+    @Test
+    public void emptyTest() throws IOException {
+        try (InputStream stream =
+                     getClass().getClassLoader().getResourceAsStream("emptyTest.txt")) {
+            SubstringSearcher alg = new SubstringSearcher(stream);
+            List<Integer> expected = List.of();
+            List<Integer> actual = alg.rabinKarp("a");
+
+            Assertions.assertEquals(expected, actual);
+        }
+    }
+}
