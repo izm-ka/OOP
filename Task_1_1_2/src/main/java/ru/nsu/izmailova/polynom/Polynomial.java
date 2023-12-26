2f4c72dfe8838b86127e7f9d8f65c57d50e7c1af
izm-ka <izm.karina@gmail.com>
2023-12-26 09:28:45 +0700

    Task_1_1_2

---
+++ Task_1_1_2/src/main/java/ru/nsu/izmailova/polynom/Polynomial.java
@@ -0,0 +1,138 @@
+package ru.nsu.izmailova.polynom;
+
+import java.util.Arrays;
+
+public class Polynomial {
+    private int[] coefficients;
+
+    public Polynomial(int[] coefficients) {
+        if (coefficients == null || coefficients.length == 0) {
+            this.coefficients = new int[]{0};
+        }
+        this.coefficients = coefficients;
+    }
+
+    public int evaluate(int x) {
+        int result = 0;
+        int power = coefficients.length - 1;
+
+        for (int coefficient : coefficients) {
+            result += coefficient * (int) Math.pow(x, power);
+            power--;
+        }
+
+        return result;
+    }
+
+    public int length() {
+        return this.coefficients.length;
+    }
+
+    public int[] getCoefficients() {
+        return coefficients;
+    }
+
+    public Polynomial add(Polynomial other) {
+        int[] newCoeffs;
+
+        if (this.length() < other.length()) {
+            newCoeffs = other.coefficients;
+            for (int i = 0; i < this.length(); i++) {
+                newCoeffs[i] += this.coefficients[i];
+            }
+        } else {
+            newCoeffs = this.coefficients;
+            for (int i = 0; i < other.length(); i++) {
+                newCoeffs[i] += other.coefficients[i];
+            }
+        }
+        return new Polynomial(newCoeffs);
+    }
+
+    public Polynomial subtract(Polynomial other) {
+        int[] negativeCoeffs = new int[other.length()];
+        for (int i = 0; i < other.length(); i++) {
+            negativeCoeffs[i] = other.coefficients[i] * -1;
+        }
+        return this.add(new Polynomial(negativeCoeffs));
+    }
+
+    public Polynomial multiply(Polynomial other) {
+        int newSize = this.length() + other.length() - 1;
+        int[] newCoeffs = new int[newSize];
+        for (int i = 0; i < other.length(); i++) {
+            for (int j = 0; j < this.length(); j++) {
+                newCoeffs[i + j] += (other.coefficients[i] * this.coefficients[j]);
+            }
+        }
+        return new Polynomial(newCoeffs);
+    }
+
+    public  Polynomial differentiate(int order) {
+        if (order <= 0) {
+            return this;
+        }
+
+        if (this.length() - order <= 0) {
+            return new Polynomial(new int[]{0});
+        }
+
+        int[] differentiateCoeffs = new int[coefficients.length - 1];
+
+        for (int i = 0; i < differentiateCoeffs.length; i++) {
+            differentiateCoeffs[i] = coefficients[i] * (coefficients.length - 1 - i);
+        }
+
+        Polynomial differentiatePolynomial = new Polynomial(differentiateCoeffs);
+
+        return differentiatePolynomial.differentiate(order - 1);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (other == null || getClass() != other.getClass()) {
+            return false;
+        }
+        Polynomial result = (Polynomial) other;
+        return Arrays.equals(coefficients, result.coefficients);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(coefficients);
+    }
+
+    @Override
+    public String toString() {
+        int power = this.length();
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < this.length(); i++) {
+            if (this.coefficients[i] != 0) {
+                if (power != this.length()) {
+                    if (this.coefficients[i] > 0) {
+                        result.append(" + ");
+                    } else {
+                        result.append(" - ");
+                    }
+                }
+                result.append(Math.abs(this.coefficients[i]));
+                power--;
+                if (power > 1) {
+                    result.append("x^");
+                    result.append(power);
+                } else if (power == 1) {
+                    result.append("x");
+                }
+            } else {
+                power--;
+            }
+        }
+        return result.toString();
+    }
+}
---
+++ Task_1_1_2/src/test/java/ru/nsu/izmailova/polynom/PolynomialTest.java
@@ -0,0 +1,145 @@
+package ru.nsu.izmailova.polynom;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+
+public class PolynomialTest {
+
+    @Test
+    public void voidEvaluate() {
+        Polynomial p = new Polynomial(new int[]{1, 2, 3});
+        assertEquals(6, p.evaluate(1));
+        assertEquals(14, p.evaluate(2));
+    }
+    
+    @Test
+    public void voidGetCoefficients() {
+        int[] coefficients = {1, 2, 3};
+        Polynomial p = new Polynomial(coefficients);
+        assertEquals(coefficients, p.getCoefficients());
+    }
+    
+    @Test
+    void simpleSum() {
+        Polynomial p1 = new Polynomial(new int[]{1, 2, 3});
+        Polynomial p2 = new Polynomial(new int[]{3, 2, 1});
+        assertEquals(p1.add(p2), new Polynomial(new int[]{4, 4, 4}));
+    }
+
+    @Test
+    void diffLengthSum() {
+        Polynomial p1 = new Polynomial(new int[]{1, 2});
+        Polynomial p2 = new Polynomial(new int[]{3, 2, 1});
+        assertEquals(p1.add(p2), new Polynomial(new int[]{4, 4, 1}));
+    }
+
+    @Test
+    void emptyCoeffsSum() {
+        Polynomial p1 = new Polynomial(new int[]{});
+        Polynomial p2 = new Polynomial(new int[]{3, 2, 1});
+        assertEquals(p1.add(p2), p2);
+    }
+
+    @Test
+    void lowerZeroSum() {
+        Polynomial p1 = new Polynomial(new int[]{0, 0, 0, 1});
+        Polynomial p2 = new Polynomial(new int[]{3, -2, -1});
+        assertEquals(p1.add(p2), new Polynomial(new int[]{3, -2, -1, 1}));
+    }
+
+    @Test
+    void simpleSub() {
+        Polynomial p1 = new Polynomial(new int[]{1, 2, 3});
+        Polynomial p2 = new Polynomial(new int[]{3, 2, 1});
+        assertEquals(p1.subtract(p2), new Polynomial(new int[]{-2, 0, 2}));
+    }
+
+    @Test
+    void lowerZeroSub() {
+        Polynomial p1 = new Polynomial(new int[]{0, 0, 0, 1});
+        Polynomial p2 = new Polynomial(new int[]{3, -2, -1});
+        assertEquals(p1.subtract(p2), new Polynomial(new int[]{-3, 2, 1, -1}));
+    }
+
+    @Test
+    void selfSub() {
+        Polynomial p = new Polynomial(new int[]{3, -2, -1});
+        assertEquals(p.subtract(p), new Polynomial(new int[]{0}));
+    }
+
+    @Test
+    void simpleMlt() {
+        Polynomial p1 = new Polynomial(new int[]{0, 0, 0, 5});
+        Polynomial p2 = new Polynomial(new int[]{1, 2, 3});
+        assertEquals(p1.multiply(p2), new Polynomial(new int[]{0, 0, 0, 5, 10, 15})
+        );
+    }
+
+    @Test
+    void emptyMlt() {
+        Polynomial p1 = new Polynomial(new int[]{});
+        Polynomial p2 = new Polynomial(new int[]{3, -2, 1});
+        assertEquals(p1.multiply(p2), new Polynomial(new int[]{0})
+        );
+    }
+
+    @Test
+    void simpleDiff() {
+        Polynomial p1 = new Polynomial(new int[]{1, 2, 3});
+        assertEquals(p1.differentiate(1), new Polynomial(new int[]{2, 6})
+        );
+    }
+
+    @Test
+    void highPowerDiff() {
+        Polynomial p1 = new Polynomial(new int[]{7, 6, 5, 4, 3, 2, 1});
+        assertEquals(p1.differentiate(5), new Polynomial(new int[]{240, 720})
+        );
+    }
+
+    @Test
+    void powerZeroDiff() {
+        Polynomial p1 = new Polynomial(new int[]{7, 6, 5, 4, 3, 2, 1});
+        assertEquals(p1.differentiate(100), new Polynomial(new int[]{0})
+        );
+    }
+
+    @Test
+    void negativeDiff() {
+        Polynomial p1 = new Polynomial(new int[]{2, 1});
+        assertEquals(p1.differentiate(1), new Polynomial(new int[]{2}));
+    }
+
+    @Test
+    void simpleEquals() {
+        Polynomial p1 = new Polynomial(new int[]{1, 2, 3});
+        Polynomial p2 = new Polynomial(new int[]{1, 2, 3});
+        assertTrue(p1.equals(p2));
+    }
+
+    @Test
+    void falseEquals() {
+        Polynomial p1 = new Polynomial(new int[]{1, 2, 3});
+        Polynomial p2 = new Polynomial(new int[]{-1, -2, -3});
+        assertFalse(p1.equals(p2));
+    }
+
+    @Test
+    void emptyEquals() {
+        Polynomial p1 = new Polynomial(new int[]{1, 2, 3});
+        Polynomial p2 = new Polynomial(new int[]{});
+        assertFalse(p1.equals(p2));
+    }
+
+    @Test
+    void voidToString() {
+        Polynomial p = new Polynomial(new int[]{1, 2, 3});
+        assertEquals("1x^2 + 2x + 3", p.toString());
+    }
+}
+
