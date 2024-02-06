5b51a7a4f0b3541ea2d82f2b1aa721aa202263d4
izm-ka <izm.karina@gmail.com>
2024-02-06 20:03:42 +0700

    delete extra dirs

--- Task_1_1_1/script.bat
+++
@@ -1,3 +0,0 @@
-javac src/main/java/ru/nsu/izmailova/heapsort/Heapsort.java -d ./build
-javadoc -d build/docs -sourcepath src/main/java -subpackages ru.nsu.izmailova.heapsort
-java -cp ./build ru.nsu.izmailova.heapsort.Heapsort
