b78570f3e8aa65dc58001889ff12842a987b1f6a
Karina <izm.karina@gmail.com>
2024-04-01 16:24:07 +0700

    add jsonHandler test

---
+++ Task_2_2_1/src/test/java/ru/nsu/izmailova/pizzeria/JsonHandlerTest.java
@@ -0,0 +1,23 @@
+package ru.nsu.izmailova.pizzeria;
+
+import org.junit.jupiter.api.Test;
+import ru.nsu.izmailova.json.JsonPizzeria;
+import ru.nsu.izmailova.json.JsonHandler;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class JsonHandlerTest {
+    @Test
+    public void testJsonHandler() throws IOException {
+        JsonHandler jsonHandler = new JsonHandler();
+        JsonPizzeria pizzeria = jsonHandler.jsonHandle();
+
+        assertEquals(5, pizzeria.getBakersAmount());
+        assertEquals(3, pizzeria.getDeliverersAmount());
+        assertEquals(20, pizzeria.getStorageSize());
+        assertEquals(100, pizzeria.getOrdersDelay());
+        assertEquals(60, pizzeria.getWorkingTime());
+    }
+}
